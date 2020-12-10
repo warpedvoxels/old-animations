14081e0d471a9d8c11154b35f3fea462a5bada01
nekkan <nxkkdev@gmail.com>
2020-12-10 16:17:59 -0300

    Add fabric repository plugin management at settings.gradle.kts.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,13 @@
 rootProject.name = "old-animations"
 enableFeaturePreview("GRADLE_METADATA")
+
+pluginManagement {
+    repositories {
+        jcenter()
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+        gradlePluginPortal()
+    }
+}
