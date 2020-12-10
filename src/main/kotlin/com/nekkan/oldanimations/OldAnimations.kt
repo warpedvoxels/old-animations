3327c7f7cbf4f526dbe0d6b6013ccfed1b3f4047
nekkan <nxkkdev@gmail.com>
2020-12-10 17:15:30 -0300

    Add up-to-date message at OldAnimations.kt.

--- src/main/kotlin/com/nekkan/oldanimations/OldAnimations.kt
+++ src/main/kotlin/com/nekkan/oldanimations/OldAnimations.kt
@@ -1,11 +1,13 @@
 @file:JvmName("OldAnimations")
 package com.nekkan.oldanimations
 
+const val OLD_ANIMATIONS_GITHUB_REPOSITORY = "https://github.com/nekkan/old-animations"
+
 /**
  * This code will be obviously updated later. This single-line `println` code will print when the Fabric mod start up.
  * This means that this code will be run only to functionality testing purposes.
  */
 fun init() {
-    println("Hello from Old Animations!")
+    println("Old Animations is up-to-date! Latest version: 1.0-SNAPSHOT for 1.16.x $OLD_ANIMATIONS_GITHUB_REPOSITORY")
 }
 
