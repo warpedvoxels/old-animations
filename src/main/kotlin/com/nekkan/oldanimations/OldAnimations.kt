97bb2f6d64e7a5eeba17a2d0f359ccfd1c83a3b0
nekkan <nxkkdev@gmail.com>
2020-12-10 17:13:50 -0300

    Create OldAnimations.kt.

---
+++ src/main/kotlin/com/nekkan/oldanimations/OldAnimations.kt
@@ -0,0 +1,11 @@
+@file:JvmName("OldAnimations")
+package com.nekkan.oldanimations
+
+/**
+ * This code will be obviously updated later. This single-line `println` code will print when the Fabric mod start up.
+ * This means that this code will be run only to functionality testing purposes.
+ */
+fun init() {
+    println("Hello from Old Animations!")
+}
+
