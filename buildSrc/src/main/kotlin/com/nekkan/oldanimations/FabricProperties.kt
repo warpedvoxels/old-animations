3f053800c77bba0be3700c98fbd2c33b97e032f2
nekkan <nxkkdev@gmail.com>
2020-12-10 16:02:59 -0300

    Create FabricProperties.kt.

---
+++ buildSrc/src/main/kotlin/com/nekkan/oldanimations/FabricProperties.kt
@@ -0,0 +1,15 @@
+package com.nekkan.oldanimations
+
+object FabricProperties: BuildProperties {
+
+    const val MINECRAFT_VERSION = "1.16.4"
+
+    const val YARN_MAPPINGS_VERSION = "1.16.4+build.7"
+
+    const val FABRIC_LOADER_VERSION = "0.10.8"
+
+    const val FABRIC_VERSION = "0.28.0+1.16"
+
+    const val LOOM_VERSION = "0.5-SNAPSHOT"
+
+}
