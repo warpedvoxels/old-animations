0f8e51d0392644febd17d29c853131a73f24b998
nekkan <nxkkdev@gmail.com>
2020-12-10 16:52:09 -0300

    Set up Fabric plugins and dependencies at build.gradle.kts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,24 @@
 import com.nekkan.oldanimations.*
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.language.jvm.tasks.ProcessResources
 
 plugins {
     kotlin("jvm") version com.nekkan.oldanimations.KotlinProperties.VERSION
+    id("fabric-loom") version com.nekkan.oldanimations.FabricProperties.LOOM_VERSION
+    java
 }
 
 repositories {
     mavenCentral()
+    maven { url = uri("http://maven.fabricmc.net/") }
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:${FabricProperties.MINECRAFT_VERSION}")
+    mappings("net.fabricmc:yarn:${FabricProperties.YARN_MAPPINGS_VERSION}:v2")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${FabricProperties.FABRIC_VERSION}")
+    modImplementation("net.fabricmc:fabric-loader:${FabricProperties.FABRIC_LOADER_VERSION}")
+    modImplementation("net.fabricmc:fabric-language-kotlin:${KotlinProperties.FABRIC_KOTLIN_VERSION}")
 }
 
 tasks.withType<KotlinCompile> {
