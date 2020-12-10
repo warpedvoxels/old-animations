363e549ccfffe43681f3e9d37ac8b016b95ea67d
nekkan <nxkkdev@gmail.com>
2020-12-10 17:53:07 -0300

    Make OldAnimations.kt log their message with a Log4J logger.

--- src/main/kotlin/com/nekkan/oldanimations/OldAnimations.kt
+++ src/main/kotlin/com/nekkan/oldanimations/OldAnimations.kt
@@ -1,13 +1,24 @@
 @file:JvmName("OldAnimations")
 package com.nekkan.oldanimations
 
-const val OLD_ANIMATIONS_GITHUB_REPOSITORY = "https://github.com/nekkan/old-animations"
+import org.apache.logging.log4j.LogManager
+
+const val OLD_ANIMATIONS_REPOSITORY = "https://github.com/nekkan/old-animations"
+
+object OldAnimations {
+    val logger = LogManager.getFormatterLogger("com.nekkan.oldanimations.OldAnimations")
+}
 
 /**
  * This code will be obviously updated later. This single-line `println` code will print when the Fabric mod start up.
  * This means that this code will be run only to functionality testing purposes.
  */
 fun init() {
-    println("Old Animations is up-to-date! Latest version: 1.0-SNAPSHOT for 1.16.x $OLD_ANIMATIONS_GITHUB_REPOSITORY")
+    /**
+     * Set the `kotlinx.coroutines.debug` property to `on` to allow Minecraft to output coroutine debug information.
+     */
+    System.setProperty("kotlinx.coroutines.debug", "on")
+
+    OldAnimations.logger.info("Old Animations is up-to-date! Latest version: 1.0-SNAPSHOT for 1.16.x $OLD_ANIMATIONS_REPOSITORY")
 }
 
