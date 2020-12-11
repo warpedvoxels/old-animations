9143104859fbc7d236286c1d3032bef4b165c7ff
nekkan <nxkkdev@gmail.com>
2020-12-11 13:45:35 -0300

    Set EventPublisher as required field at Event.kt.

--- src/main/kotlin/com/nekkan/oldanimations/event/Event.kt
+++ src/main/kotlin/com/nekkan/oldanimations/event/Event.kt
@@ -2,6 +2,8 @@ package com.nekkan.oldanimations.event
 
 sealed class Event {
 
+    abstract val publisher: EventPublisher
+
     /**
      * Executes before a call (HEAD) of a Mixin injection.
      */
