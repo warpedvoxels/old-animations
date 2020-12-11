2476870cd45d8a1f263fc19e941d436cb7e26d6c
nekkan <nxkkdev@gmail.com>
2020-12-11 13:49:30 -0300

    Make CallbackInfo a required field at Event.kt.

--- src/main/kotlin/com/nekkan/oldanimations/event/Event.kt
+++ src/main/kotlin/com/nekkan/oldanimations/event/Event.kt
@@ -1,8 +1,12 @@
 package com.nekkan.oldanimations.event
 
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
+
 sealed class Event {
 
     abstract val publisher: EventPublisher
+
+    abstract val callbackInfo: CallbackInfo
 
     /**
      * Executes before a call (HEAD) of a Mixin injection.
