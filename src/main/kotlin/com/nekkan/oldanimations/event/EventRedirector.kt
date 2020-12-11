557ff0f437c6f7012caa569487faca6ca6c4b62e
nekkan <nxkkdev@gmail.com>
2020-12-11 13:04:05 -0300

    Create EventRedirector.kt.

---
+++ src/main/kotlin/com/nekkan/oldanimations/event/EventRedirector.kt
@@ -0,0 +1,24 @@
+package com.nekkan.oldanimations.event
+
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.onEach
+import kotlin.reflect.KClass
+
+class EventRedirector: SharedFlowBasedEventPublisher, SharedFlowBasedEventSubscriber {
+
+    private val _flow = MutableSharedFlow<Event>()
+
+    override val flow: SharedFlow<Event>
+        get() = _flow
+
+    override suspend fun executeFor(event: Event) {
+        return _flow.emit(event)
+    }
+
+    override fun <T: Event> subscribe(forClass: KClass<T>, callback: (T) -> Unit) {
+        flow.filter { forClass.isInstance(it) }.onEach { callback(it as T) }
+    }
+
+}
