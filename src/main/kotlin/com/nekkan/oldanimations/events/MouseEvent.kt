2297ac89c58c72928672788ee92730923d67f96b
nekkan <nxkkdev@gmail.com>
2020-12-11 13:11:49 -0300

    Make MouseEvent.kt extend Event.Pre.

--- src/main/kotlin/com/nekkan/oldanimations/events/MouseEvent.kt
+++ src/main/kotlin/com/nekkan/oldanimations/events/MouseEvent.kt
@@ -2,21 +2,21 @@ package com.nekkan.oldanimations.events
 
 import com.nekkan.oldanimations.event.Event
 
-sealed class MouseEvent {
+sealed class MouseEvent: Event.Pre() {
 
     /**
      * An event that will be executed when the scroll (middle key) be pressed.
      */
-    object Middle: Event.Pre()
+    object Middle: MouseEvent()
 
     /**
      * An event that will be executed when the left click be pressed.
      */
-    object Left: Event.Pre()
+    object Left: MouseEvent()
 
     /**
      * An event that will be executed when the right click be pressed.
      */
-    object Right: Event.Pre()
+    object Right: MouseEvent()
 
 }
