44eb8c6db32b0dfd6f367ec67164829deb18a95c
nekkan <nxkkdev@gmail.com>
2020-12-11 14:30:38 -0300

    Create FlowBasedEventSubscriber#with extension function at extensions.kt.

--- src/main/kotlin/com/nekkan/oldanimations/extensions.kt
+++ src/main/kotlin/com/nekkan/oldanimations/extensions.kt
@@ -4,15 +4,22 @@ package com.nekkan.oldanimations
 
 import com.nekkan.oldanimations.event.Event
 import com.nekkan.oldanimations.event.FlowBasedEventSubscriber
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.filterIsInstance
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.withContext
 import java.net.URL
 
-inline fun <reified T: Event> FlowBasedEventSubscriber.onEach(noinline callback: (T) -> Unit) = flow
+inline fun <reified T: Event> FlowBasedEventSubscriber.onEach(crossinline callback: (T) -> Unit) = flow
     .filterIsInstance<T>()
     .onEach { callback(it) }
+
+inline fun <reified T: Event> FlowBasedEventSubscriber.with(
+    coroutineScope: CoroutineScope,
+    crossinline callback: (T) -> Unit
+) = onEach<T>(callback).launchIn(coroutineScope)
 
 suspend inline fun URL.get() = withContext(Dispatchers.IO) {
     readText(Charsets.UTF_8)
