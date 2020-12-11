e7834ed023b8fede087198ebba539b7005b90a8b
nekkan <nxkkdev@gmail.com>
2020-12-11 13:31:44 -0300

    Create GET request function at extensions.kt.

--- src/main/kotlin/com/nekkan/oldanimations/extensions.kt
+++ src/main/kotlin/com/nekkan/oldanimations/extensions.kt
@@ -6,7 +6,17 @@ import com.nekkan.oldanimations.event.Event
 import com.nekkan.oldanimations.event.FlowBasedEventSubscriber
 import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.onEach
+import java.net.URL
 
 inline fun <reified T: Event> FlowBasedEventSubscriber.onEach(noinline callback: (T) -> Unit) = flow
     .filterIsInstance<T>()
     .onEach { callback(it) }
+
+inline fun URL.get(): String? {
+    return try {
+        readText(Charsets.UTF_8)
+    } catch(exception: Throwable) {
+        OldAnimations.error(exception)
+        null
+    }
+}
