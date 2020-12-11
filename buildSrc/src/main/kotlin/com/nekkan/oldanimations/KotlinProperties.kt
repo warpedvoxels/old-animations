51f3a88115fdae458f2f6d108fbd0d06cb178f5a
nekkan <nxkkdev@gmail.com>
2020-12-11 16:48:33 -0300

    Update Kotlin to 1.4.21 at KotlinProperties.kt.

--- buildSrc/src/main/kotlin/com/nekkan/oldanimations/KotlinProperties.kt
+++ buildSrc/src/main/kotlin/com/nekkan/oldanimations/KotlinProperties.kt
@@ -2,7 +2,7 @@ package com.nekkan.oldanimations
 
 object KotlinProperties: BuildProperties {
 
-    const val VERSION = "1.4.20"
+    const val VERSION = "1.4.21"
 
     const val FABRIC_KOTLIN_VERSION = "1.4.0+build.1"
 
