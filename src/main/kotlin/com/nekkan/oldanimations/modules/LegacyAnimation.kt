f1a7bc3a2137fe3e85b2f6785f7e51586a7cf6f3
nekkan <nxkkdev@gmail.com>
2020-12-11 20:25:49 -0300

    Make isEnabled a checkbox at LegacyAnimation.kt.

--- src/main/kotlin/com/nekkan/oldanimations/modules/LegacyAnimation.kt
+++ src/main/kotlin/com/nekkan/oldanimations/modules/LegacyAnimation.kt
@@ -1,13 +1,14 @@
 package com.nekkan.oldanimations.modules
 
 import com.nekkan.oldanimations.event.Event
+import com.nekkan.oldanimations.settings.Checkbox
 import com.nekkan.oldanimations.settings.Identifier
 
 interface LegacyAnimation<T: Event> {
 
     val identifier: Identifier
 
-    val isEnabled: Boolean
+    val isEnabled: Checkbox
 
     fun disable() { /* no-op */
     }
