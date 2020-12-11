074927684c7e4ccfc83938444ac01795afb48ef2
nekkan <nxkkdev@gmail.com>
2020-12-11 20:55:19 -0300

    Add ID parameter to Checkbox.kt.

--- src/main/kotlin/com/nekkan/oldanimations/settings/Checkbox.kt
+++ src/main/kotlin/com/nekkan/oldanimations/settings/Checkbox.kt
@@ -2,6 +2,7 @@ package com.nekkan.oldanimations.settings
 
 data class Checkbox(
     override val name: String,
+    override val id: String,
     override val description: String,
     val isCheckedByDefault: Boolean = false,
     val isChecked: Boolean = isCheckedByDefault,
