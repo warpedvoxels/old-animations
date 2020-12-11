104538f83f3beae6284e80e51d3891d631f740b1
nekkan <nxkkdev@gmail.com>
2020-12-11 13:48:59 -0300

    Set event redirector variable at PlayerCombatEvent.kt.

--- src/main/kotlin/com/nekkan/oldanimations/events/PlayerCombatEvent.kt
+++ src/main/kotlin/com/nekkan/oldanimations/events/PlayerCombatEvent.kt
@@ -1,13 +1,22 @@
 package com.nekkan.oldanimations.events
 
 import com.nekkan.oldanimations.event.Event
+import com.nekkan.oldanimations.event.EventPublisher
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.player.PlayerEntity
 
 abstract class PlayerCombatEvent {
 
-    open class Pre(val attacker: PlayerEntity, val attacked: LivingEntity): Event.Pre()
+    open class Pre(
+        val attacker: PlayerEntity,
+        val attacked: LivingEntity,
+        override val publisher: EventPublisher
+    ): Event.Pre()
 
-    open class Post(val attacker: PlayerEntity, val attacked: LivingEntity): Event.Post()
+    open class Post(
+        val attacker: PlayerEntity,
+        val attacked: LivingEntity,
+        override val publisher: EventPublisher
+    ): Event.Post()
 
 }
