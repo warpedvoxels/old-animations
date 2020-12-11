6c721dcc2cac698c7580968755a1c8c57e6aae45
nekkan <nxkkdev@gmail.com>
2020-12-11 20:15:23 -0300

    Create T#Identifier extension function at Identifier.kt.

--- src/main/kotlin/com/nekkan/oldanimations/settings/Identifier.kt
+++ src/main/kotlin/com/nekkan/oldanimations/settings/Identifier.kt
@@ -4,3 +4,5 @@ data class Identifier(
     override val name: String,
     override val description: String
 ): Setting
+
+inline fun <reified T: Any> T.Identifier(description: String) = Identifier(this::class.simpleName!!, description)
