28a0bd8a18b045901d0d69b2befb2ec6211669e0
nekkan <nxkkdev@gmail.com>
2020-12-11 13:50:56 -0300

    Add callbackInfo parameter at PlayerCombatEvent.kt.

--- src/main/kotlin/com/nekkan/oldanimations/events/PlayerCombatEvent.kt
+++ src/main/kotlin/com/nekkan/oldanimations/events/PlayerCombatEvent.kt
@@ -4,19 +4,22 @@ import com.nekkan.oldanimations.event.Event
 import com.nekkan.oldanimations.event.EventPublisher
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.player.PlayerEntity
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
 
 abstract class PlayerCombatEvent {
 
     open class Pre(
         val attacker: PlayerEntity,
         val attacked: LivingEntity,
-        override val publisher: EventPublisher
+        override val publisher: EventPublisher,
+        override val callbackInfo: CallbackInfo
     ): Event.Pre()
 
     open class Post(
         val attacker: PlayerEntity,
         val attacked: LivingEntity,
-        override val publisher: EventPublisher
+        override val publisher: EventPublisher,
+        override val callbackInfo: CallbackInfo
     ): Event.Post()
 
 }
