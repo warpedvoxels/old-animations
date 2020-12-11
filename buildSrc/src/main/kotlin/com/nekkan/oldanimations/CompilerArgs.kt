c79b0f9dce02a281297ac44d41a782188124d90b
nekkan <nxkkdev@gmail.com>
2020-12-11 20:59:49 -0300

    Create CompilerArgs.kt at buildSrc.

---
+++ buildSrc/src/main/kotlin/com/nekkan/oldanimations/CompilerArgs.kt
@@ -0,0 +1,11 @@
+package com.nekkan.oldanimations
+
+object CompilerArgs {
+
+    const val X_OPT_IN = "-Xopt-in=kotlin.RequiresOptIn"
+
+    override fun toString(): String {
+        return X_OPT_IN
+    }
+
+}
