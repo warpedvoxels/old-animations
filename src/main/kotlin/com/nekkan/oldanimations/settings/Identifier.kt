f62758c0b16e2cffe891a6c955c6fa3a71186b82
nekkan <nxkkdev@gmail.com>
2020-12-11 20:56:07 -0300

    Add ID parameter to Identifier.kt.

--- src/main/kotlin/com/nekkan/oldanimations/settings/Identifier.kt
+++ src/main/kotlin/com/nekkan/oldanimations/settings/Identifier.kt
@@ -2,7 +2,10 @@ package com.nekkan.oldanimations.settings
 
 data class Identifier(
     override val name: String,
+    override val id: String,
     override val description: String
 ): Setting
 
-inline fun <reified T: Any> T.Identifier(description: String) = Identifier(this::class.simpleName!!, description)
+inline fun <reified T: Any> T.Identifier(id: String, description: String) = Identifier(
+    this::class.simpleName!!, id, description
+)
