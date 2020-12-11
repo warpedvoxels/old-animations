6afb56a39c278cad2b5137bccde63277de5d3633
nekkan <nxkkdev@gmail.com>
2020-12-11 13:50:33 -0300

    Add callbackInfo parameter at MouseEvent.kt.

--- src/main/kotlin/com/nekkan/oldanimations/events/MouseEvent.kt
+++ src/main/kotlin/com/nekkan/oldanimations/events/MouseEvent.kt
@@ -2,22 +2,32 @@ package com.nekkan.oldanimations.events
 
 import com.nekkan.oldanimations.event.Event
 import com.nekkan.oldanimations.event.EventPublisher
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
 
 sealed class MouseEvent: Event.Pre() {
 
     /**
      * An event that will be executed when the scroll (middle key) be pressed.
      */
-    data class Middle(override val publisher: EventPublisher): MouseEvent()
+    data class Middle(
+        override val publisher: EventPublisher,
+        override val callbackInfo: CallbackInfo
+    ): MouseEvent()
 
     /**
      * An event that will be executed when the left click be pressed.
      */
-    data class Left(override val publisher: EventPublisher): MouseEvent()
+    data class Left(
+        override val publisher: EventPublisher,
+        override val callbackInfo: CallbackInfo
+    ): MouseEvent()
 
     /**
      * An event that will be executed when the right click be pressed.
      */
-    data class Right(override val publisher: EventPublisher): MouseEvent()
+    data class Right(
+        override val publisher: EventPublisher,
+        override val callbackInfo: CallbackInfo
+    ): MouseEvent()
 
 }
