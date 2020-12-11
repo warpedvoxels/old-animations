6376fdb2d4b9631c4060132575b84d9b8ac3b604
nekkan <nxkkdev@gmail.com>
2020-12-11 12:56:30 -0300

    Create flow based implementations at EventSubscriber.kt.

--- src/main/kotlin/com/nekkan/oldanimations/event/EventSubscriber.kt
+++ src/main/kotlin/com/nekkan/oldanimations/event/EventSubscriber.kt
@@ -1,9 +1,23 @@
 package com.nekkan.oldanimations.event
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlin.reflect.KClass
 
 interface EventSubscriber {
 
     fun <T: Any> subscribe(forClass: KClass<T>, callback: (T) -> Unit)
+
+}
+
+interface FlowBasedEventSubscriber: EventSubscriber {
+
+    val flow: Flow<Event>
+
+}
+
+interface SharedFlowBasedEventSubscriber: FlowBasedEventSubscriber {
+
+    override val flow: SharedFlow<Event>
 
 }
