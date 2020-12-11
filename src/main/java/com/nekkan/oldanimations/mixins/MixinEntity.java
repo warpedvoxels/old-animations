ab3e9add8c98904842bf727aa86864e74200dac8
nekkan <nxkkdev@gmail.com>
2020-12-11 19:59:57 -0300

    Create MixinEntity.java.

---
+++ src/main/java/com/nekkan/oldanimations/mixins/MixinEntity.java
@@ -0,0 +1,21 @@
+package com.nekkan.oldanimations.mixins;
+
+import com.nekkan.oldanimations.OldAnimations;
+import com.nekkan.oldanimations.event.EventPublisher;
+import com.nekkan.oldanimations.events.ToggleSneakingEvent;
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Entity.class)
+public class MixinEntity {
+
+    @Inject(at = @At("HEAD"), method = "setSneaking")
+    private void setSneaking(boolean sneaking, CallbackInfo callbackInfo) {
+        EventPublisher publisher = OldAnimations.getRedirector();
+        OldAnimations.redirect(new ToggleSneakingEvent(sneaking, publisher, callbackInfo));
+    }
+
+}
