53337aa06324068c4f62ab49d7641567a16662e0
nekkan <nxkkdev@gmail.com>
2020-12-11 13:32:55 -0300

    Make get request function run with Dispatchers.IO context at extensions.kt.

--- src/main/kotlin/com/nekkan/oldanimations/extensions.kt
+++ src/main/kotlin/com/nekkan/oldanimations/extensions.kt
@@ -4,19 +4,16 @@ package com.nekkan.oldanimations
 
 import com.nekkan.oldanimations.event.Event
 import com.nekkan.oldanimations.event.FlowBasedEventSubscriber
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.withContext
 import java.net.URL
 
 inline fun <reified T: Event> FlowBasedEventSubscriber.onEach(noinline callback: (T) -> Unit) = flow
     .filterIsInstance<T>()
     .onEach { callback(it) }
 
-inline fun URL.get(): String? {
-    return try {
-        readText(Charsets.UTF_8)
-    } catch(exception: Throwable) {
-        OldAnimations.error(exception)
-        null
-    }
+suspend inline fun URL.get() = withContext(Dispatchers.IO) {
+    readText(Charsets.UTF_8)
 }
