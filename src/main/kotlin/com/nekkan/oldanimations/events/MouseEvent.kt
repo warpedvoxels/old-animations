8278f388eaeff91d25e2a2233f77254517128474
nekkan <nxkkdev@gmail.com>
2020-12-11 13:46:34 -0300

    Add publisher parameter at MouseEvent.kt.

--- src/main/kotlin/com/nekkan/oldanimations/events/MouseEvent.kt
+++ src/main/kotlin/com/nekkan/oldanimations/events/MouseEvent.kt
@@ -1,22 +1,23 @@
 package com.nekkan.oldanimations.events
 
 import com.nekkan.oldanimations.event.Event
+import com.nekkan.oldanimations.event.EventPublisher
 
 sealed class MouseEvent: Event.Pre() {
 
     /**
      * An event that will be executed when the scroll (middle key) be pressed.
      */
-    object Middle: MouseEvent()
+    data class Middle(override val publisher: EventPublisher): MouseEvent()
 
     /**
      * An event that will be executed when the left click be pressed.
      */
-    object Left: MouseEvent()
+    data class Left(override val publisher: EventPublisher): MouseEvent()
 
     /**
      * An event that will be executed when the right click be pressed.
      */
-    object Right: MouseEvent()
+    data class Right(override val publisher: EventPublisher): MouseEvent()
 
 }
