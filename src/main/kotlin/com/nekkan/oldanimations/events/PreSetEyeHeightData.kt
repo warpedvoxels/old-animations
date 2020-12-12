65d47897d08ecc8d03b3954bee1a0e2daa7e33be
nekkan <nxkkdev@gmail.com>
2020-12-12 01:54:04 -0300

    Create PreSetEyeHeightData.kt.

---
+++ src/main/kotlin/com/nekkan/oldanimations/events/PreSetEyeHeightData.kt
@@ -0,0 +1,6 @@
+package com.nekkan.oldanimations.events
+
+import net.minecraft.entity.EntityDimensions
+import net.minecraft.entity.EntityPose
+
+data class PreSetEyeHeightData(val pose: EntityPose, val dimension: EntityDimensions)
