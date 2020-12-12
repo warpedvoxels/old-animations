195652819bf8682b4de83e53e3e00b199af915a8
nekkan <nxkkdev@gmail.com>
2020-12-12 02:50:11 -0300

    Make Checkbox.kt checked by default.

--- src/main/kotlin/com/nekkan/oldanimations/settings/Checkbox.kt
+++ src/main/kotlin/com/nekkan/oldanimations/settings/Checkbox.kt
@@ -5,7 +5,7 @@ import com.nekkan.oldanimations.modules.LegacyAnimation
 data class Checkbox(
     override val name: String,
     override val description: String,
-    val isCheckedByDefault: Boolean = false,
+    val isCheckedByDefault: Boolean = true,
     val isChecked: Boolean = isCheckedByDefault,
     val isLocked: Boolean = false
 ): Setting
