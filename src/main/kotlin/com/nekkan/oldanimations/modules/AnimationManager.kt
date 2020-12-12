ad8c60c9c1278aef06b53ff154652dc7222aa45c
nekkan <nxkkdev@gmail.com>
2020-12-12 07:09:32 -0300

    Disable event redirecting at AnimationManager.kt.

--- src/main/kotlin/com/nekkan/oldanimations/modules/AnimationManager.kt
+++ src/main/kotlin/com/nekkan/oldanimations/modules/AnimationManager.kt
@@ -2,50 +2,34 @@
 
 package com.nekkan.oldanimations.modules
 
-import com.nekkan.oldanimations.coroutineScope
-import com.nekkan.oldanimations.event.Event
-import com.nekkan.oldanimations.eventRedirector
-import com.nekkan.oldanimations.with
 import kotlin.reflect.KClass
 import kotlin.reflect.KType
 import kotlin.reflect.full.createType
 import kotlin.reflect.typeOf
 
-internal class AnimationManager(private val _collection: MutableMap<KType, LegacyAnimation<*, *>>) {
+internal class AnimationManager(private val _collection: MutableMap<KType, LegacyAnimation>) {
 
-    val collection: Map<KType, LegacyAnimation<*, *>> get() = _collection
+    val collection: Map<KType, LegacyAnimation> get() = _collection
 
-    private fun <T: Event, R: Any> getOrNull(type: KType): LegacyAnimation<T, R>? {
-        return _collection[type] as? LegacyAnimation<T, R>?
-    }
+    private fun getOrNull(type: KType) = _collection[type]
 
-    operator fun <T: Event, R: Any> get(type: KType): LegacyAnimation<T, R> {
+    operator fun get(type: KType): LegacyAnimation {
         return getOrNull(type) ?: error("Invalid animation or type does not match.")
     }
 
-    inline operator fun <reified T: Event> set(type: KType, animation: LegacyAnimation<T, Any>?) {
+    operator fun set(type: KType, animation: LegacyAnimation?) {
         if(animation == null) {
             _collection.remove(type)
             return
         }
         _collection[type] = animation
-        redirect(type, animation)
     }
 
 }
 
-private inline fun <reified T: Event> redirect(type: KType, animation: LegacyAnimation<T, Any>) {
-    eventRedirector.with<T>(coroutineScope) {
-        animation.execute(it)
-    }
-}
-
-internal inline fun <T: Event, R: Any> AnimationManager.get(kClass: KClass<T>) = get<T, R>(kClass.createType())
+internal inline fun <T: LegacyAnimation> AnimationManager.get(kClass: KClass<T>) = get(kClass.createType())
 
 @OptIn(ExperimentalStdlibApi::class)
-internal inline fun <reified T: Event, R: Any> AnimationManager.get() = get<T, R>(typeOf<T>())
-
-@OptIn(ExperimentalStdlibApi::class)
-internal inline fun <reified T: Event> AnimationManager.set(animation: LegacyAnimation<T, out Any>?) {
-    return set(typeOf<T>(), animation as? LegacyAnimation<T, Any>?)
+internal inline fun <reified T: LegacyAnimation> AnimationManager.set(animation: T?) {
+    return set(typeOf<T>(), animation)
 }
