2d1f91ef8c686b09c636bae760ec3bc03feb5a31
nekkan <nxkkdev@gmail.com>
2020-12-11 21:46:54 -0300

    Add clientPlayerEntity getter variable at extensions.kt.

--- src/main/kotlin/com/nekkan/oldanimations/extensions.kt
+++ src/main/kotlin/com/nekkan/oldanimations/extensions.kt
@@ -10,6 +10,8 @@ import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.withContext
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.network.ClientPlayerEntity
 import java.net.URL
 
 inline fun <reified T: Event> FlowBasedEventSubscriber.onEach(crossinline callback: (T) -> Unit) = flow
@@ -20,6 +22,9 @@ inline fun <reified T: Event> FlowBasedEventSubscriber.with(
     coroutineScope: CoroutineScope,
     crossinline callback: (T) -> Unit
 ) = onEach<T>(callback).launchIn(coroutineScope)
+
+internal inline val clientPlayerEntity: ClientPlayerEntity
+    get() = MinecraftClient.getInstance().player ?: error("Cannot get the client player in their current state.")
 
 suspend inline fun URL.get() = withContext(Dispatchers.IO) {
     readText(Charsets.UTF_8)
