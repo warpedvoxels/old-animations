9215985f6303bab2fa1d3f2e1ddcaf133aba8180
nekkan <nxkkdev@gmail.com>
2020-12-12 02:02:05 -0300

    Delete PreSetEyeHeightData.kt.

--- src/main/kotlin/com/nekkan/oldanimations/events/PreSetEyeHeightData.kt
+++
@@ -1,6 +0,0 @@
-package com.nekkan.oldanimations.events
-
-import net.minecraft.entity.EntityDimensions
-import net.minecraft.entity.EntityPose
-
-data class PreSetEyeHeightData(val pose: EntityPose, val dimension: EntityDimensions)
