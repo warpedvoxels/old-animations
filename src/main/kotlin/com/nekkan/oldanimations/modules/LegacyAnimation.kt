cd1184a92972b0ae200150de773b8fcc62b1e0d6
nekkan <nxkkdev@gmail.com>
2020-12-13 18:31:40 -0300

    Add ScreenSetting annotation by default to isEnabled at LegacyAnimation.kt.

--- src/main/kotlin/com/nekkan/oldanimations/modules/LegacyAnimation.kt
+++ src/main/kotlin/com/nekkan/oldanimations/modules/LegacyAnimation.kt
@@ -2,11 +2,13 @@ package com.nekkan.oldanimations.modules
 
 import com.nekkan.oldanimations.settings.Checkbox
 import com.nekkan.oldanimations.settings.Identifier
+import com.nekkan.oldanimations.settings.ScreenSetting
 
 interface LegacyAnimation {
 
     val identifier: Identifier
 
+    @ScreenSetting(isDefault = true)
     val isEnabled: Checkbox
 
     fun disable() {
