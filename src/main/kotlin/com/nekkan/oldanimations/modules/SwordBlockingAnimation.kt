0f082bc8b93228d2c5f6c2b0b4f1574a34de311e
nekkan <nxkkdev@gmail.com>
2020-12-13 21:38:14 -0300

    Fix identifier at SwordBlockingAnimation.kt companion.

--- src/main/kotlin/com/nekkan/oldanimations/modules/SwordBlockingAnimation.kt
+++ src/main/kotlin/com/nekkan/oldanimations/modules/SwordBlockingAnimation.kt
@@ -1,9 +1,9 @@
 package com.nekkan.oldanimations.modules
 
-import com.nekkan.oldanimations.MinecraftIdentifier
 import net.fabricmc.fabric.api.`object`.builder.v1.client.model.FabricModelPredicateProviderRegistry
 import net.minecraft.item.Item
 import net.minecraft.item.Items
+import net.minecraft.util.Identifier
 
 class SwordBlockingAnimation: LegacyAnimation {
 
@@ -11,7 +11,7 @@ class SwordBlockingAnimation: LegacyAnimation {
     //    get() = Identifier("swordblocking", "")
 
     companion object {
-        fun registerFor(item: Item, identifier: MinecraftIdentifier) {
+        fun registerFor(item: Item, identifier: Identifier) {
             FabricModelPredicateProviderRegistry.register(item, identifier) { _, _, entity ->
                 if(entity != null && entity.offHandStack.item == Items.SHIELD && entity.isUsingItem) 1f else 0f
             }
