924465eea3b15e6b3157a6cd2e50b7d24f69a63a
nekkan <nxkkdev@gmail.com>
2020-12-13 21:31:22 -0300

    Create ModMenuIntegration.java.

---
+++ src/main/java/com/nekkan/oldanimations/ModMenuIntegration.java
@@ -0,0 +1,13 @@
+package com.nekkan.oldanimations;
+
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
+import io.github.prospector.modmenu.api.ModMenuApi;
+import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
+
+public class ModMenuIntegration implements ModMenuApi {
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> AutoConfig.getConfigScreen(OldAnimationsConfig.class, parent).get();
+    }
+}
