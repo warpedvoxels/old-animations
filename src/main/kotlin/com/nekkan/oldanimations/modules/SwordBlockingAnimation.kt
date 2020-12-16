6a9eb605ed6979f9a1beda371c426fc5aab8d61e
nekkan <nxkkdev@gmail.com>
2020-12-15 21:52:27 -0300

    Add registerWithShieldFor function in SwordBlockingAnimation.kt.

--- src/main/kotlin/com/nekkan/oldanimations/modules/SwordBlockingAnimation.kt
+++ src/main/kotlin/com/nekkan/oldanimations/modules/SwordBlockingAnimation.kt
@@ -8,6 +8,12 @@ import net.minecraft.util.Identifier
 object SwordBlockingAnimation: LegacyAnimation {
 
     fun registerFor(item: Item, identifier: Identifier) {
+        FabricModelPredicateProviderRegistry.register(item, identifier) { stack, _, entity ->
+            if(entity != null && entity.isUsingItem && entity.activeItem == stack) 1f else 0f
+        }
+    }
+
+    fun registerWithShieldFor(item: Item, identifier: Identifier) {
         FabricModelPredicateProviderRegistry.register(item, identifier) { _, _, entity ->
             if(entity != null && entity.offHandStack.item == Items.SHIELD && entity.isUsingItem) 1f else 0f
         }
