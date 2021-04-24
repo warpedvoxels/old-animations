ca79cbb7095219740753f69da41204b20bc2ef1b
nekkan <nxkkdev@gmail.com>
2021-04-24 18:13:13 -0300

    feat: Create OldAnimationsFeature.java

---
+++ src/main/java/gq/nkkx/oldanimations/features/OldAnimationsFeature.java
@@ -0,0 +1,18 @@
+package gq.nkkx.oldanimations.features;
+
+import gq.nkkx.oldanimations.features.context.FeatureExecutionContext;
+
+/**
+ * A feature is a class by which you alter the visual mechanics of the game to port an old animation to new versions.
+ * Each resource implements this interface or its derivatives.
+ *
+ * @see ItemRenderingFeature
+ * @since 2.0.0-SNAPSHOT
+ */
+public interface OldAnimationsFeature<T extends FeatureExecutionContext> {
+
+    boolean isEnabled();
+
+    void transform(T context);
+
+}
