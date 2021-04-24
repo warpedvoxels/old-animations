eedf63fb27b24a279f7332bfcb33095b6283b2ce
nekkan <nxkkdev@gmail.com>
2021-04-24 14:30:15 -0300

    Initial commit

---
+++ src/main/java/gq/nkkx/oldanimations/OldAnimations.java
@@ -0,0 +1,12 @@
+package gq.nkkx.oldanimations;
+
+import net.fabricmc.api.ModInitializer;
+
+public class OldAnimations implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+
+}
---
+++ src/main/java/gq/nkkx/oldanimations/OldAnimationsClient.java
@@ -0,0 +1,15 @@
+package gq.nkkx.oldanimations;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class OldAnimationsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        // no-op
+    }
+
+}
