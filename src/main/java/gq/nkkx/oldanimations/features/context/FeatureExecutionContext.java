c89d5f3c447efee7d8a3ced762577443b3005012
nekkan <nxkkdev@gmail.com>
2021-04-24 18:12:09 -0300

    feat: Create FeatureExecutionContext.java

---
+++ src/main/java/gq/nkkx/oldanimations/features/context/FeatureExecutionContext.java
@@ -0,0 +1,32 @@
+package gq.nkkx.oldanimations.features.context;
+
+import gq.nkkx.oldanimations.OldAnimations;
+import gq.nkkx.oldanimations.features.OldAnimationsFeature;
+import gq.nkkx.oldanimations.options.OldAnimationsOptions;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * The context needed to perform a transformation on a feature.
+ *
+ * @see OldAnimationsFeature
+ * @since 2.0.0-SNAPSHOT
+ */
+public interface FeatureExecutionContext {
+
+    static FeatureExecutionContext create(PlayerEntity player, OldAnimationsOptions options, CallbackInfo callbackInfo) {
+        return new DefaultFeatureExecutionContext(player, options, callbackInfo);
+    }
+
+    static FeatureExecutionContext create(CallbackInfo callbackInfo) {
+        return create(MinecraftClient.getInstance().player, OldAnimations.options(), callbackInfo);
+    }
+
+    PlayerEntity player();
+
+    OldAnimationsOptions options();
+
+    CallbackInfo callbackInfo();
+
+}
